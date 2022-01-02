69065282c8685fd4da0b01d5e3d054dd0f91c302
ferney <ferneyh57@gmail.com>
2022-01-01 21:17:23 -0500

    estructura de la api y pruebas docker

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-print('hello')
+from fastapi import FastAPI
+from starlette.responses import RedirectResponse
+from pydantic import BaseModel
+
+app = FastAPI()
+
+items = []
+
+class post(BaseModel):
+    id: str
+    name: str
+    
+
+@app.get("/")
+def index():
+    return RedirectResponse(url="/docs/")
+
+@app.get("/partes")
+def get_root():
+    return items
+
+@app.post("/")
+def post_root(post:post):
+    return RedirectResponse(url="/docs/")
+
+@app.put("/")
+def put_root():
+    return RedirectResponse(url="/docs/")
+
+@app.delete("/")
+def delete_root():
+    return RedirectResponse(url="/docs/")
+
